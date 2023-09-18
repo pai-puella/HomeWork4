aad983580edf1dbcdd01c04c6967c57934dffcd4
pai-puella <cat.dyshkant@yandex.ru>
2023-09-18 14:19:13 +0300

    Added task 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задача 1");
+
+        int age = 22;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
         }
     }

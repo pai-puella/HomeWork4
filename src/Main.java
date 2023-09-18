100a85658c333b69c22925f35833cd8a558bccd5
pai-puella <cat.dyshkant@yandex.ru>
2023-09-18 14:40:00 +0300

    Added task 4

--- src/Main.java
+++ src/Main.java
@@ -32,5 +32,22 @@ public class Main {
         } else {
             System.out.println("Если скорость " + speed + " км/ч, то можно ездить спокойно");
         }
+
+        System.out.println();
+        System.out.println("Задача 4");
+        int humanAge = 14;
+
+        if (humanAge >= 2 && humanAge <= 6) {
+            System.out.println("Если возраст человека равен " + humanAge + ", то ему нужно ходить в детский сад");
+        } else if (humanAge >= 7 && humanAge <= 17) {
+            System.out.println("Если возраст человека равен " + humanAge + ", то ему нужно ходить в школу");
+        } else if (humanAge >= 18 && humanAge <= 24) {
+            System.out.println("Если возраст человека равен " + humanAge + ", то его место в университете");
+        } else {
+            System.out.println("Если возраст человека равен " + humanAge + ", то ему пора ходить на работу");
+        }
+
+        System.out.println();
+        System.out.println("Задача 5");
         }
     }

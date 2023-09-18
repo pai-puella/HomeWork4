f5154bb30f49fd1b7a7c36ea4a185cfaa849f32c
pai-puella <cat.dyshkant@yandex.ru>
2023-09-18 14:29:22 +0300

    Added task 2

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,16 @@ public class Main {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
         }
 
+        System.out.println();
+        System.out.println("Задача 2");
+        int tempOut = 14;
+
+        if (tempOut < 5) {
+            System.out.println("На улице холодно, нужно надеть шапку");
+        } else {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        }
+
+        System.out.println("На улице " + tempOut + " градусов");
         }
     }
